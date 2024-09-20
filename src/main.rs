57b786640372c3a54f34cd3d6b34bc4551316ebf
jackcat13 <christophehenry11@gmail.com>
2024-09-20 16:45:04 +0200

    feat: create custom unit tests framework to be executed over the OS code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,79 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    panic!("kozdapdkzaopd");
-    println!("Hello World {}", "I am a nested string :D");
+    println!("Welcome to the JackCatOs :)");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!();
+    serial_println!("STARTING TESTS !!!");
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
