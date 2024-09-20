424254e8208152aa3ef6c1a07b6ec232c33fceb9
jackcat13 <christophehenry11@gmail.com>
2024-09-20 12:01:45 +0200

    feat: init commit with minimal kernel printing text
    feat: add writer abstraction to write new lines to the OS

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    panic!("kozdapdkzaopd");
+    println!("Hello World {}", "I am a nested string :D");
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
