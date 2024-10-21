eabe7d38c186bd5ec4986a95048773a87bb32738
jackcat13 <christophehenry11@gmail.com>
2024-10-21 11:00:56 +0200

    feat: setup integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,31 +1,26 @@
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(jackcatos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-#![no_std]
-#![no_main]
 
 use core::panic::PanicInfo;
+
 use jackcatos::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Welcome to the JackCatOs :)");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     jackcatos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
