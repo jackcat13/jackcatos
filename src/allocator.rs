7762d32821328511fe1e79cbbcb6b9e38fd5fbbf
christophe.henry <christophe.henry@galenica.com>
2024-10-23 15:46:21 +0200

    feat: Implement custom bump allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,12 +1,15 @@
 use alloc::alloc::{GlobalAlloc, Layout};
+use bump::BumpAllocator;
 use linked_list_allocator::LockedHeap;
 use core::ptr::null_mut;
+
+pub mod bump;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new()); 
 
 use x86_64::{
     structures::paging::{
@@ -42,4 +45,26 @@ pub fn init_heap(
     }
     
     Ok(())
+}
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
