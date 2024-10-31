0ccf02774f4748f3a49eed39504736558225b3ea
jackcat13 <christophehenry11@gmail.com>
2024-10-31 09:58:27 +0100

    feat: Cooperative multitasking support and keyboard async processing

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use jackcatos::{
     allocator, hlt_loop,
     memory::{self, BootInfoFrameAllocator},
-    println,
+    println, task::{executor::Executor, keyboard, Task},
 };
 use x86_64::VirtAddr;
 
@@ -28,13 +27,25 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("Heap init failed");
-    let x = Box::new(41);
-
+    
     #[cfg(test)]
     test_main();
+    
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 
-    println!("It did not crash!");
     hlt_loop();
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
 
 #[cfg(not(test))]
